5975ab558306287fa249950bad7bb9aaa9ba0a5a
duremika <bogdanov.mv.95@gmail.com>
2021-01-15 22:36:07 +0600

    update

--- game.py
+++ game.py
@@ -9,26 +9,38 @@ offset = 3
 CONTINUE = "CONTINUE"
 SETTINGS = "SETTINGS"
 
-header = "press 'q' for exit"
-start = None
+age = 0
 
-def print_background(stdscr, box):
+header = "press <q> for exit, <m> for change mode"
+modes = ['time', 'eat', 'drink', 'play', 'wash']
+mode = 0
+
+def print_background(stdscr, box, mode):
     stdscr.clear()
     stdscr.addstr(0, offset+1, header)
-    age = datetime.now() - start
-    age = age.seconds
-    stdscr.addstr(1, offset+1, str(age))
+
+
+    if mode == modes[0]:
+        line = str(age) + " years old"
+    elif mode == modes[1]:
+        line = "press <y> for give food"
+    stdscr.addstr(1, offset+1, line)
+
     textpad.rectangle(stdscr, *map(int, box))
 
 
-def egg(stdscr, box):
-    with open("sprites/egg.txt") as f:
-        lines = f.readlines()
+def change_mode(_modes):
+    global mode
+    mode += 1
+    if mode >= len(_modes):
+        mode = 0
 
+
+def draw(stdscr,box, lines):
     w = box[3] + box[1]
     x = w // 2 - len(lines[0]) // 2
-    lines.reverse()
     i = 0
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_WHITE)
     for row in lines:
         j = 0
         row = row.strip()
@@ -36,20 +48,53 @@ def egg(stdscr, box):
             y = box[2] - offset // 2 - i
             if ch == '_':
                 ch = ' '
+            elif ch == '+':
+                ch = '.'
             else:
-                ch = '*'
+                stdscr.attron(curses.color_pair(1))
+                ch = '#'
 
             stdscr.addstr(y, x+j, ch)
+            stdscr.attroff(curses.color_pair(1))
             j += 1
         i += 1
 
-def idle(stdscr, box):
-    pass
+
+def egg(stdscr, box, need_change_mode):
+    _modes = modes[:1]
+    if need_change_mode:
+        change_mode(_modes)
+
+    print_background(stdscr, box, _modes[mode])
+
+    with open("sprites/egg.txt") as f:
+        lines = f.readlines()
+
+    lines.reverse()
+    draw(stdscr, box, lines)
+
+def baby(stdscr, box, need_change_mode):
+    _modes = modes[:5]
+    if need_change_mode:
+        change_mode(_modes)
+
+    print_background(stdscr, box, _modes[mode])
+
+    with open("sprites/baby_1.txt") as f:
+        lines = f.readlines()
+
+    lines.reverse()
+    draw(stdscr, box, lines)
+
+
+def update_age():
+    delta = datetime.now() - start
+    global age
+    age = delta.seconds // 3 #(3600 * 8)
 
 
 def main(stdscr):
     state = menu.main(stdscr)
-
     global start
     start = datetime.now()
 
@@ -63,16 +108,22 @@ def main(stdscr):
 
     height, width = stdscr.getmaxyx()
     box = (offset+1, offset, height - offset, width - offset)
+
     state = egg
 
     while 1:
         key = stdscr.getch()
+        need_change_mode = False
         if key == ord("q"):
             break
+        elif key == ord("m"):
+            need_change_mode = True
 
-        print_background(stdscr, box)
-        state(stdscr, box)
-        #stdscr.refresh()
+        update_age()
+        if age == 1:
+            state = baby
+
+        state(stdscr, box,  need_change_mode)
 
 
 if __name__ == '__main__':
